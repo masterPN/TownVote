1eb49e585c47500bd70529f1c887eb5357e44ef0
masterPN <69779985+masterPN@users.noreply.github.com>
2022-05-07 20:06:59 +0700

    Set route for api

--- src/main.go
+++ src/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetTest(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "The is a message from voter!"})
+}
+
+func main() {
+	router := gin.Default()
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+
+	router.GET("/", GetTest)
+
+	router.Run()
+}
